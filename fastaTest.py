a6793a833827c6c0d6b92ef1e70e665e2898bb87
Andrew DiCarlo <andrew.dicarlo@gmail.com>
2021-12-06 21:30:07 -0600

    fully removed pysam

--- fastaTest.py
+++ fastaTest.py
@@ -2,12 +2,10 @@
 
 from collections import Counter
 import pnaMotifs as pm
-from pysam import FastaFile
 from Bio import SeqIO
 
 
 fasta = "masked.fasta"
-seq_obj = FastaFile(fasta)
 
 dArr = []
 bgCount = Counter()
@@ -20,7 +18,6 @@ for record in SeqIO.parse(fasta, "fasta"):
 total = sum([bgCount[x] for x in bgCount if x != b'N'])
 
 bg = {x: bgCount[x]/total for x in bgCount}
-bg.update({78: .001})
 for k in range(4, 13):
     pwm = pm.motifEMOOPS(dArr, k, bg)
     print('--------------')
